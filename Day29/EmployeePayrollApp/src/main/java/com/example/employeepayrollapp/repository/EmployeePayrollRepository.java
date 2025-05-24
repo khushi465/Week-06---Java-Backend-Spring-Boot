572e0be66dff96b600087d37262c73b47378872c
Khushi465 <gamedevak1213@gmail.com>
2025-05-24 12:18:58 +0530

    Added day5

--- Day29/EmployeePayrollApp/src/main/java/com/example/employeepayrollapp/repository/EmployeePayrollRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeepayrollapp.repository;
-
-import com.example.employeepayrollapp.model.EmployeePayrollData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer> {
-}
